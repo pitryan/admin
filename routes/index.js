581a66b23ea544dcce80a396bda079730afa857d
pitryan <pitryan.535210015@stu.untar.ac.id>
2022-11-08 21:28:49 +0700

    edit path

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,32 @@ router.get('/', async(req, res) => {
 
 });
 
+router.get('/Pesanan/Belum-Bayar', (req, res) => {
+    res.render('pages/html/Pesanan/Belum-Bayar')
+});
+
+router.get('/Pesanan/Kode-Pembayaran-Retail', (req, res) => {
+    res.render('pages/html/Pesanan/Kode-Pembayaran-Retail')
+});
+
+router.get('/Pesanan/Selesai', (req, res) => {
+    res.render('pages/html/Pesanan/Selesai')
+});
+
+router.get('/Pesanan/Semua-pesanan', (req, res) => {
+    res.render('pages/html/Pesanan/Semua-pesanan')
+});
+
+router.get('/Pesanan/Siap-Dikirim', (req, res) => {
+    res.render('pages/html/Pesanan/Siap-Dikirim')
+});
+
+router.get('/Pesanan/Sudah-Bayar', (req, res) => {
+    res.render('pages/html/Pesanan/Sudah-Bayar')
+});
+
+router.get('/views/pages', (req, res) => {
+    res.render('pages/index')
+});
+
 module.exports = router;
